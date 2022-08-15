5630bc0c2c160e0127fed27351d3751557e54efa
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 21:38:49 +0200

    IfStatement

---
+++ ControlFLowAndConditionals/IfStatement.java
@@ -0,0 +1,12 @@
+package ControlFLowAndConditionals;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isCold = false; //true or false
+
+        if (isCold) {
+            //executes ONLY if isCold is true
+            System.out.println("It's cold, wear a coat!");
+        }
+    }
+}
