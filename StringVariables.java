9ed65ef968c8b6da927e57617c34be4650ca653e
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 17:37:07 +0200

    StringVariables

---
+++ StringVariables.java
@@ -0,0 +1,15 @@
+public class StringVariables {
+    public static void main(String[] args) {
+        String driver;
+        driver = "Vin Diesel";
+        driver = driver.toUpperCase();
+        int letters = driver.length();
+        System.out.println(letters);
+        System.out.println(driver);
+
+        driver = driver.toLowerCase();
+        System.out.println(driver);
+
+    }
+
+}
