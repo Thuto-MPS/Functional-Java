473b29258793de907857de8c7f7a721318db0f25
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 21:39:13 +0200

    LogicalOperators

---
+++ ControlFLowAndConditionals/LogicalOperators.java
@@ -0,0 +1,16 @@
+package ControlFLowAndConditionals;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int ticketPrice = 10;
+        int age = 15;
+        boolean isStudent = false;
+
+        if(age <= 15 || age > 60 || isStudent) {
+            ticketPrice = 5;
+        } else {
+            ticketPrice = 10;
+        }
+        System.out.println(ticketPrice);
+    }
+}
