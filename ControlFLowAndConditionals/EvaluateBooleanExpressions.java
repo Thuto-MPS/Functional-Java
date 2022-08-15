a6e0d5ee5673076361a257be4afc513e541aee97
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 21:38:35 +0200

    EvaluateBooleanExpressions

---
+++ ControlFLowAndConditionals/EvaluateBooleanExpressions.java
@@ -0,0 +1,13 @@
+package ControlFLowAndConditionals;
+
+public class EvaluateBooleanExpressions {
+    public static void main (String[] args) {
+        int x = 20;
+
+        boolean a = x <= 21;
+        boolean b = 2*2 > 8;
+        boolean c = 5 != x;
+        boolean d = 20 == x;
+
+    }
+}
