d055293c5dc66652f3f354af9546be0f2b27c712
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 18:24:20 +0200

    Data types.

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int maxInt = 2147483647; //10 digit number.
+        long largerThanInt = 2147483647*100000000; //larger numbers
+        double fraction = 99.275; //decimal number or fractional number
+        String FullText = "(b) Jef Bezos was the first person " +
+                "to be worth < $100 billion"; //text
+        char answer = 'b'; //single character or alphabet
+        char three = '3'; //single alphabet or number
+
+        boolean fact = true; //true or false
+
+        boolean conditon = false; //true or false
+
+    }
+
+}
+
