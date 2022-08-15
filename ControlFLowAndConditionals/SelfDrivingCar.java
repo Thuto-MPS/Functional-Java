20b09b3890dd66208cc393f8d389b9daab09dcad
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 21:39:28 +0200

    SelfDrivingCar

---
+++ ControlFLowAndConditionals/SelfDrivingCar.java
@@ -0,0 +1,17 @@
+package ControlFLowAndConditionals;
+
+public class SelfDrivingCar {
+    public static void main(String[] args){
+        /*Self-driving car that recognise
+        simple traffic lights.*/
+    boolean isLightGreen = true;
+
+    if(isLightGreen){
+        //if robot is green, drive.
+        System.out.println("Drive");
+    }
+
+
+    }
+
+}
