5182e78fa6698eabdfe0f47d19af6d67201fd193
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 21:38:19 +0200

    ElseStatement

---
+++ ControlFLowAndConditionals/ElseStatement.java
@@ -0,0 +1,15 @@
+package ControlFLowAndConditionals;
+
+public class ElseStatement {
+    public static void main(String[] args) {
+        boolean isLightGreen = false;
+
+        if (isLightGreen) {
+            //traffic light is green.
+            System.out.println("Drive!");
+        } else {
+            //light is NOT green.
+            System.out.println("Stop!");
+        }
+    }
+}
