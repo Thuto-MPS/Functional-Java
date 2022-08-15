2c12f67b12f3bae9906dc3554363ac59ff12ffa0
tserapel021 <tserapel021@student.wethinkcode.co.za>
2022-08-15 18:37:36 +0200

    ALl the math

---
+++ AllTheMath.java
@@ -0,0 +1,23 @@
+public class AllTheMath {
+    public static void main(String[] args) {
+        int x = 2 + 3; //addition
+        int y = 4 - 5; //subtraction
+        int temp = -20; //negative number
+        int days = 7 * 4; //multiplication
+        double div = 5/2; //division
+        double accurate = 5/2.0; //accurate division
+        System.out.println(div);
+        System.out.println(accurate);
+
+        //Variable Arithmetic
+        int a = 2+3; // = 5
+        int b = 4-5; // = -1
+        int c = a*b; // = -5
+        System.out.println(c);
+
+        double paid = 10;
+        double change = 3.25;
+        double tip =(paid-change)*0.2;
+
+    }
+}
